d0a956a9b4fe266d5397955573b8a0b69de9e5c6
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-07-10 14:15:01 +0300

    fix: update button disable logic and refactor Lock contract
    - Updated the button disable logic in AccountForm to ensure it is only disabled when there is no account or when deposits/withdrawals are loading.
    - Refactored the Lock contract into a TimeLock contract, implementing deposit and withdraw functions with locking mechanism for 24 hours.
    - Added events for deposit and withdrawal actions, and included functions to retrieve balance and lock time for users.

--- client/src/components/account-form/index.tsx
+++ client/src/components/account-form/index.tsx
@@ -89,7 +89,7 @@ export const AccountForm = () => {
         </button>
         <button
           onClick={handleWithdrawFunds}
-          disabled={depositDisabled || withdrawLoading}
+          disabled={!account || depositLoading || withdrawLoading}
           className={twMerge(
             "bg-blue-500 text-white px-10 py-2 mt-4 rounded-md cursor-pointer enabled:hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed",
             animationStyle
